fd9a7d47482627fce691a3c82eac538df8e2031e
geist_2501 <blaironbute@gmail.com>
2024-03-02 13:43:50 +0000

    Fix database connections

--- ToDoIt.Server/Database/PostgresCommandExecutor.cs
+++ ToDoIt.Server/Database/PostgresCommandExecutor.cs
@@ -5,13 +5,14 @@ namespace ToDoIt.Server.Database;
 
 public class PostgresCommandExecutor : ICommandExecutor
 {
-    private const string c_ConnectionUrl = "Host=localhost;Username=bx_admin;Password=nojXGZDdTeTuj3ZrcYYE"; // TODO (BC) replace.
+    private const string c_ConnectionUrl = "Host=localhost;Username=bx_admin;Password=nojXGZDdTeTuj3ZrcYYE;Database=todoit;"; // TODO (BC) replace.
 
     private readonly NpgsqlDataSource m_NpgsqlDataSource = NpgsqlDataSource.Create(c_ConnectionUrl);
 
     public async Task Execute(Func<NpgsqlCommand, Task> command)
     {
         await using var connection = m_NpgsqlDataSource.CreateConnection();
+        await connection.OpenAsync();
         await using var cmd = connection.CreateCommand();
         
         await command(cmd);
@@ -20,6 +21,7 @@ public class PostgresCommandExecutor : ICommandExecutor
     public async Task<T> Execute<T>(Func<NpgsqlCommand, Task<T>> command)
     {
         await using var connection = m_NpgsqlDataSource.CreateConnection();
+        await connection.OpenAsync();
         await using var cmd = connection.CreateCommand();
         
         return await command(cmd);
