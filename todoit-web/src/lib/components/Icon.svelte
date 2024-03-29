69915782c03846f213d11178df096dd23fdee4bd
geist_2501 <blaironbute@gmail.com>
2024-03-29 15:35:37 +0000

    Added icon component

---
+++ todoit-web/src/lib/components/Icon.svelte
@@ -0,0 +1,27 @@
+﻿<script lang="ts">
+    import type {Icon} from "../icon-definition";
+    import {iconList} from "../icon-definition";
+
+    export let iconName: Icon;
+    
+    let selected;
+    $: selected = iconList[iconName];
+</script>
+
+<span class="icon" aria-label={selected.alt}>{selected.symbol}</span>
+
+<style>
+    .icon {
+        font-family: 'Material Symbols Outlined';
+        font-weight: normal;
+        font-style: normal;
+        font-size: 24px;  /* Preferred icon size */
+        display: inline-block;
+        line-height: 1;
+        text-transform: none;
+        letter-spacing: normal;
+        word-wrap: normal;
+        white-space: nowrap;
+        direction: ltr;
+    }
+</style>
---
+++ todoit-web/src/lib/icon-definition.ts
@@ -0,0 +1,14 @@
+﻿type IconList = Record<string, { symbol: string, alt: string }>;
+
+export const iconList: IconList = {
+    'ellipsis': {
+        symbol: '&#e5d3;',
+        alt: 'more'
+    },
+    'edit': {
+        symbol: '&#e3c9;',
+        alt: 'edit'
+    }
+}
+
+export type Icon = keyof typeof iconList;
